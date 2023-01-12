0db0337e0a35163cbab0cbc1f30d89983de43848
Martin <martinmallolcc@gmail.com>
2023-01-12 15:58:44 -0300

    New codes, so they can be searched on the API object

--- roadtrip-gas-cost/src/utils/states.js
+++ roadtrip-gas-cost/src/utils/states.js
@@ -3,203 +3,261 @@
 const statesUSA = [
     {
         name: "Alabama",
-        abbreviation:"AL"
+        abbreviation:"AL",
+        petroleum: "R30",
     },
     {
         name:"Alaska",
-        abbreviation:"AK"
+        abbreviation:"AK",
+        petroleum: "R50",
     },
     {
         name:"Arizona",
-        abbreviation:"AZ"
+        abbreviation:"AZ",
+        petroleum: "R50",
     },
     {
         name:"Arkansas",
-        abbreviation:"AR"
+        abbreviation:"AR",
+        petroleum: "R30"
     },
     {
         name:"California",
-        abbreviation:"CA"
+        abbreviation:"CA",
+        petroleum: "R50",
+        code: "SCA"
     },
     {
         name:"Colorado",
-        abbreviation:"CO"
+        abbreviation:"CO",
+        petroleum: "R40",
+        code: "SCO"
     },
     {
         name:"Connecticut",
-        abbreviation:"CT"
+        abbreviation:"CT",
+        petroleum: "R1X"
     },
     {
         name:"Delaware",
-        abbreviation:"DE"
+        abbreviation:"DE",
+        petroleum: "R1Y"
     },
     {
         name:"Florida",
-        abbreviation:"FL"
+        abbreviation:"FL",
+        petroleum: "R1Z",
+        code: "SFL"
     },
     {
         name:"Georgia",
-        abbreviation:"GA"
+        abbreviation:"GA",
+        petroleum: "R1Z"
     },
     {
         name:"Hawaii",
-        abbreviation:"HI"
+        abbreviation:"HI",
+        petroleum: "R50"
     },
     {
         name:"Idaho",
-        abbreviation:"ID"
+        abbreviation:"ID",
+        petroleum: "R40"
     },
     {
         name:"Illinois",
-        abbreviation:"IL"
+        abbreviation:"IL",
+        petroleum: "R20"
     },
     {
         name:"Indiana",
-        abbreviation:"IN"
+        abbreviation:"IN",
+        petroleum: "R20"
     },
     {
         name:"Iowa",
-        abbreviation:"IA"
+        abbreviation:"IA",
+        petroleum: "R20"
     },
     {
         name:"Kansas",
-        abbreviation:"KS"
+        abbreviation:"KS",
+        petroleum: "R20"
     },
     {
         name:"Kentucky",
-        abbreviation:"KY"
+        abbreviation:"KY",
+        petroleum: "R20"
     },
     {
         name:"Louisiana",
-        abbreviation:"LA"
+        abbreviation:"LA",
+        petroleum: "R30"
     },
     {
         name:"Maine",
-        abbreviation:"ME"
+        abbreviation:"ME",
+        petroleum: "R1X"
     },
     {
         name:"Maryland",
-        abbreviation:"MD"
+        abbreviation:"MD",
+        petroleum: "R1Y"
     },
     {
         name:"Massachusetts",
-        abbreviation:"MA"
+        abbreviation:"MA",
+        petroleum: "R1X",
+        code: "SMA"
     },
     {
         name:"Michigan",
-        abbreviation:"MI"
+        abbreviation:"MI",
+        petroleum: "R20"
     },
     {
         name:"Minnesota",
-        abbreviation:"MN"
+        abbreviation:"MN",
+        petroleum: "R20",
+        code: "SMN"
     },
     {
         name:"Mississippi",
-        abbreviation:"MS"
+        abbreviation:"MS",
+        petroleum: "R20"
     },
     {
         name:"Missouri",
-        abbreviation:"MO"
+        abbreviation:"MO",
+        petroleum: "R20"
     },
     {
         name:"Montana",
-        abbreviation:"MT"
+        abbreviation:"MT",
+        petroleum: "R40"
     },
     {
         name:"Nebraska",
-        abbreviation:"NE"
+        abbreviation:"NE",
+        petroleum: "R20"
     },
     {
         name:"Nevada",
-        abbreviation:"NV"
+        abbreviation:"NV",
+        petroleum: "R50"
     },
     {
         name:"New Hampshire",
-        abbreviation:"NH"
+        abbreviation:"NH",
+        petroleum: "R1X"
     },
     {
         name:"New Jersey",
-        abbreviation:"NJ"
+        abbreviation:"NJ",
+        petroleum: "R1Y"
     },
     {
         name:"New Mexico",
-        abbreviation:"NM"
+        abbreviation:"NM",
+        petroleum: "R30"
     },
     {
         name:"New York",
-        abbreviation:"NY"
+        abbreviation:"NY",
+        petroleum: "R1Y",
+        code: "SNY"
     },
     {
         name:"North Carolina",
-        abbreviation:"NC"
+        abbreviation:"NC",
+        petroleum: "R1Z"
     },
     {
         name:"North Dakota",
-        abbreviation:"ND"
+        abbreviation:"ND",
+        petroleum: "R20"
     },
     {
         name:"Ohio",
-        abbreviation:"OH"
+        abbreviation:"OH",
+        petroleum: "R20",
+        code: "SOH"
     },
     {
         name:"Oklahoma",
-        abbreviation:"OK"
+        abbreviation:"OK",
+        petroleum: "R20"
     },
     {
         name:"Oregon",
-        abbreviation:"OR"
+        abbreviation:"OR",
+        petroleum: "R50"
     },
     {
         name:"Pennsylvania",
-        abbreviation:"PA"
+        abbreviation:"PA",
+        petroleum: "R1Y"
     },
     {
         name:"Rhode Island",
-        abbreviation:"RI"
+        abbreviation:"RI",
+        petroleum: "R1X"
     },
     {
         name:"South Carolina",
-        abbreviation:"SC"
+        abbreviation:"SC",
+        petroleum: "R1Z"
     },
     {
         name:"South Dakota",
-        abbreviation:"SD"
+        abbreviation:"SD",
+        petroleum: "R20"
     },
     {
         name:"Tennessee",
-        abbreviation:"TN"
+        abbreviation:"TN",
+        petroleum: "R20"
     },
     {
         name:"Texas",
-        abbreviation:"TX"
+        abbreviation:"TX",
+        petroleum: "R30",
+        code: "STX"
     },
     {
         name:"Utah",
-        abbreviation:"UT"
+        abbreviation:"UT",
+        petroleum: "R40"
     },
     {
         name:"Vermont",
-        abbreviation:"VT"
+        abbreviation:"VT",
+        petroleum: "R1X"
     },
     {
         name:"Virginia",
-        abbreviation:"VA"
+        abbreviation:"VA",
+        petroleum: "R1Z"
     },
     {
         name:"Washington",
-        abbreviation:"WA"
+        abbreviation:"WA",
+        petroleum: "R50"
     },
     {
         name:"West Virginia",
-        abbreviation:"WV"
+        abbreviation:"WV",
+        petroleum: "R1Z"
     },
     {
         name:"Wisconsin",
-        abbreviation:"WI"
+        abbreviation:"WI",
+        petroleum: "R20"
     },
     {
         name:"Wyoming",
-        abbreviation:"WY"
+        abbreviation:"WY",
+        petroleum: "R40"
     }
 ]
 
